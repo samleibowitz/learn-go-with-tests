260ba431a8fac44a1ecfeb3935962e8595d559b5
Sam Leibowitz <s.leibowitz@tridiuum.com>
2021-10-18 20:29:49 -0400

    added an example to repeat

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,5 +1,6 @@
 package iteration
 
+// Repeat takes a string and repeats it the number of times specified in the second argument
 func Repeat(character string, repetitions int) string {
 	result := ""
 
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 
@@ -30,4 +33,10 @@ func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	result := Repeat("Giggity", 3)
+	fmt.Println(result)
+	// Output: GiggityGiggityGiggity
 }
