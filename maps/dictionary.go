b6b8c59d00b306205faf97569c813839c8fab4c9
Sam Leibowitz <s.leibowitz@tridiuum.com>
2021-11-13 09:01:50 -0500

    Dictionary#Add

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,6 +1,15 @@
 package main
 
-import "errors"
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 type Dictionary map[string]string
 
@@ -9,8 +18,23 @@ func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
