e979407026ee246243efca27c8f54e30f60d4e55
Sam Leibowitz <s.leibowitz@tridiuum.com>
2021-12-05 00:35:55 -0500

    sync v2: threadsafe!

--- sync/counter.go
+++ sync/counter.go
@@ -1,13 +1,22 @@
 package main
 
+import "sync"
+
 type Counter struct {
+	mu  sync.Mutex
 	val int
 }
 
-func (c *Counter) Inc() {
-	c.val += 1
+func NewCounter() *Counter {
+	return &Counter{}
 }
 
-func (c Counter) Value() int {
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	c.val += 1
+	c.mu.Unlock()
+}
+
+func (c *Counter) Value() int {
 	return c.val
 }
--- sync/counter_test.go
+++ sync/counter_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sync"
 	"testing"
 )
 
@@ -11,11 +12,29 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 		counter.Inc()
 
-		assertCounter(t, counter, 3)
+		assertCounter(t, &counter, 3)
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, &counter, wantedCount)
 	})
 }
 
-func assertCounter(t testing.TB, got Counter, want int) {
+func assertCounter(t testing.TB, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), want)
